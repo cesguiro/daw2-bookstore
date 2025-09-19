6573eb59e89301ee61d48777d963cef8a0db211e
César Guijarro <cesguiro@gmail.com>
2025-09-19 08:54:04 +0200

    first commit

---
+++ src/main/java/es/cesguiro/daw2_bookstore/Daw2BookstoreApplication.java
@@ -0,0 +1,13 @@
+package es.cesguiro.daw2_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw2BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw2BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cesguiro/daw2_bookstore/Daw2BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cesguiro.daw2_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw2BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
