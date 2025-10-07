da859b9ffa003e5a45bf4dfc1a05c3fbcdd26d4e
César Guijarro <cesar.rosaleny@gmail.com>
2025-10-07 19:02:39 +0200

    añadido actualización y borrado de libros

--- src/main/java/es/cesguiro/daw2_bookstore/controller/BookController.java
+++ src/main/java/es/cesguiro/daw2_bookstore/controller/BookController.java
@@ -3,10 +3,7 @@ package es.cesguiro.daw2_bookstore.controller;
 import es.cesguiro.domain.model.Page;
 import es.cesguiro.domain.service.BookService;
 import es.cesguiro.domain.service.dto.BookDto;
-import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -32,5 +29,18 @@ public class BookController {
     @PostMapping
     public BookDto createBook(@RequestBody BookDto bookDto) {
         return bookService.create(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable("id") Long id, @RequestBody BookDto bookDto) {
+        if (!id.equals(bookDto.id())) {
+            throw new IllegalArgumentException("ID in path and request body must match");
+        }
+        return bookService.update(bookDto);
+    }
+
+    @DeleteMapping("/{isbn}")
+    public void deleteBook(@PathVariable("isbn") String isbn) {
+        bookService.deleteByIsbn(isbn);
     }
 }
